b097d3309b59ee0e417b63bd73743310040e3303
nafiu <nafiushaibu1@gmail.com>
2026-01-16 13:26:31 +0000

    Bump version to 1.1.9

--- src/pydantic_mini/__init__.py
+++ src/pydantic_mini/__init__.py
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ===========================================================================================
 """
 
-__version__ = "1.1.8"
+__version__ = "1.1.9"
 
 from .base import BaseModel
 from .typing import Attrib, MiniAnnotated
